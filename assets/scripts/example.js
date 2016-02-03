8b365b469941aada5f303be0123348cc963cc55b
kyleezrakatz <kyleezrakatz@gmail.com>
2016-02-02 22:09:11 -0500

    X in the square on click, minor style edits.

--- assets/scripts/example.js
+++ assets/scripts/example.js
@@ -1,21 +1,19 @@
 'use strict';
 
 $("td").on("click", function() {
-  console.log("event object:");
+  console.log("The square was clicked.");
   console.dir(event);
-  $(event.currentTarget).off('click');
-  let inputArray = (event.currentTarget.id);
-  console.log(inputArray);
-  let i = 0;
-  for(inputArray[i] = 0; i < inputArray.length; i++) {
-    if(i % 2 === 0) {
-      let x = $("<img src='http://i.imgur.com/W9J3BBb.png'>");
-      $(event.currentTarget).append(x);
-    } else if (i % 2 !== 0) {
-      var o = $("<img src='http://i.imgur.com/u3tDsz5.png'>");
-      $(event.currentTarget).append(o);
+  $(event.currentTarget).off("click");
+  $(event.currentTarget).text("X");
+  });
+
+  function startGame() {
+    document.turn = "X";
     }
-  } 
-});
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+    startGame();
+  });
 
 module.exports = true;
