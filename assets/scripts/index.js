751beb8cefce93e198c57b7e9c8af2e783f8fa62
kyleezrakatz <kyleezrakatz@gmail.com>
2016-02-04 23:05:07 -0500

    With the gitignored I think and grunt built

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,4 +1,5 @@
 'use strict';
+
 //VARIABLES
 let turnCount = 0;
 
@@ -17,81 +18,87 @@ function playMessage(content) {
   $('#play').text(content);
 }
 
-playMessage("Play?");
+playMessage('Play?');
+
 //PLACE LETTER AND SWITCH TURNS
-$("td").on("click", function() {
-  $(event.currentTarget).text("X");
+$('td').on('click', function() {
+  $(event.currentTarget).text('X');
   if (turnCount % 2 === 0) {
     $(this).text('X');
   } else {
     $(this).text('O');
   }
+
   turnCount++;
 });
 
 let xWin = false;
 let oWin = false;
+
 //WINNER MESSAGE
 function winMessage() {
   if (xWin === true) {
-    playMessage("X Win!");
+    playMessage('X Win!');
   } else if (oWin === true) {
-    playMessage("O Wins!");
+    playMessage('O Wins!');
   }
 }
 
 function whoWins() {// CHECKS X
-  if (sq0 === sq1 && sq0 === sq2 && sq0 === "X" || //first row
-    sq3 === sq4 && sq3 === sq5 && sq3 === "X" || //second row
-    sq6 === sq7 && sq6 === sq8 && sq6 === "X" || //third row
-    sq0 === sq3 && sq0 === sq6 && sq0 === "X" || //first column
-    sq1 === sq4 && sq1 === sq7 && sq1 === "X" || //second column
-    sq2 === sq5 && sq2 === sq8 && sq2 === "X" || //third column
-    sq0 === sq4 && sq0 === sq8 && sq0 === "X" || //diagonal 1
-    sq2 === sq4 && sq2 === sq6 && sq2 === "X") //diagonal 2
+  if (sq0 === sq1 && sq0 === sq2 && sq0 === 'X' || //first row
+    sq3 === sq4 && sq3 === sq5 && sq3 === 'X' || //second row
+    sq6 === sq7 && sq6 === sq8 && sq6 === 'X' || //third row
+    sq0 === sq3 && sq0 === sq6 && sq0 === 'X' || //first column
+    sq1 === sq4 && sq1 === sq7 && sq1 === 'X' || //second column
+    sq2 === sq5 && sq2 === sq8 && sq2 === 'X' || //third column
+    sq0 === sq4 && sq0 === sq8 && sq0 === 'X' || //diagonal 1
+    sq2 === sq4 && sq2 === sq6 && sq2 === 'X') //diagonal 2
   {
     return xWin === true;
   } else {//CHECKS O
-    if (sq0 === sq1 && sq0 === sq2 && sq0 === "O" || //second row
-      sq3 === sq4 && sq3 === sq5 && sq3 === "O" ||
-      sq6 === sq7 && sq6 === sq8 && sq6 === "O" || //third row
-      sq0 === sq3 && sq0 === sq6 && sq0 === "O" || //first column
-      sq1 === sq4 && sq1 === sq7 && sq1 === "O" || //second column
-      sq2 === sq5 && sq2 === sq8 && sq2 === "O" || //third column
-      sq0 === sq4 && sq0 === sq8 && sq0 === "O" || //diagonal 1
-      sq2 === sq4 && sq2 === sq6 && sq2 === "O") //diagonal 2
+    if (sq0 === sq1 && sq0 === sq2 && sq0 === 'O' || //second row
+      sq3 === sq4 && sq3 === sq5 && sq3 === 'O' ||
+      sq6 === sq7 && sq6 === sq8 && sq6 === 'O' || //third row
+      sq0 === sq3 && sq0 === sq6 && sq0 === 'O' || //first column
+      sq1 === sq4 && sq1 === sq7 && sq1 === 'O' || //second column
+      sq2 === sq5 && sq2 === sq8 && sq2 === 'O' || //third column
+      sq0 === sq4 && sq0 === sq8 && sq0 === 'O' || //diagonal 1
+      sq2 === sq4 && sq2 === sq6 && sq2 === 'O') //diagonal 2
     {
       return oWin === true;
     } else { // CHECKS TIE
-      if (((sq0 === "X") || (sq0 === "O")) && ((sq3 === "X") ||
-          (sq3 === "O")) && ((sq6 === "X") || (sq6 === "O")) &&
-        ((sq1 === "X") || (sq1 === "O")) && ((sq4 === "X") ||
-          (sq4 === "O")) && ((sq7 === "X") || (sq7 === "O")) &&
-        ((sq2 === "X") || (sq2 === "O")) && ((sq5 === "X") ||
-          (sq5 === "O")) && ((sq8 === "X") || (sq8 === "O"))) {
-        playMessage("Stalemate!");
+      if (((sq0 === 'X') || (sq0 === 'O')) && ((sq3 === 'X') ||
+          (sq3 === 'O')) && ((sq6 === 'X') || (sq6 === 'O')) &&
+        ((sq1 === 'X') || (sq1 === 'O')) && ((sq4 === 'X') ||
+          (sq4 === 'O')) && ((sq7 === 'X') || (sq7 === 'O')) &&
+        ((sq2 === 'X') || (sq2 === 'O')) && ((sq5 === 'X') ||
+          (sq5 === 'O')) && ((sq8 === 'X') || (sq8 === 'O'))) {
+        playMessage('Stalemate!');
       }
     }
   }
 }
 
 $(document).ready(function() {
-  console.log("ready!");
+  console.log('ready!');
 });
+
 //TURNS OFF SQUARES SOMETIMES too much or not enough
-$("td").on("click", function() {
-  if ($("td").text() === true) {//too much w != null
-    $(event.currentTarget).off("click");
+$('td').on('click', function() {
+  if ($('td').text() === true) {//too much w != null
+    $(event.currentTarget).off('click');
   }
 });
- //CALLS FUNCTIONS ON BOARD CLICK
-$('#board').on("click", function() {
+
+//CALLS FUNCTIONS ON BOARD CLICK
+$('#board').on('click', function() {
   whoWins();
   winMessage();
 });
+
 //CLEARS BOARD
-$('#butt').on("click", function() {
-  $("td").text(" ");
+$('#butt').on('click', function() {
+  $('td').text(' ');
 });
 
 module.exports = true;
