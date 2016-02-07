2df6ac85a35d29977cb5bd7b484c4cd0e156a9a3
kyleezrakatz <kyleezrakatz@gmail.com>
2016-02-07 18:19:15 -0500

    Trying to get the forms to work in html and jquery

--- assets/scripts/example.js
+++ assets/scripts/example.js
@@ -4,86 +4,91 @@ const myApp = {
   baseUrl: 'http://tic-tac-toe.wdibos.com',
 };
 
-$(document).ready(() => {
-  // SIGN UP
-  $('#sign-up').on('submit', function(e) {
-    e.preventDefault();
-    var formData = new FormData(e.target);
-    $.ajax({
-      url: myApp.baseUrl + '/sign-up',
-      method: 'POST',
-      contentType: false,
-      processData: false,
-      data: formData,
-    }).done(function(data) {
-      console.log(data);
-    }).fail(function(jqxhr) {
-      console.error(jqxhr);
-    });
+// SIGN UP
+$('#sign-up').on('submit', function(e) {
+  e.preventDefault();
+  var formData = new FormData(e.target);
+  $.ajax({
+    url: myApp.baseUrl + '/sign-up',
+    method: 'POST',
+    contentType: false,
+    processData: false,
+    data: formData,
+  }).done(function(data) {
+    console.log(data);
+  }).fail(function(jqxhr) {
+    console.error(jqxhr);
   });
 });
 
 // SIGN IN
 $('#sign-in').on('submit', function(e) {
-    e.preventDefault();
-    var formData = new FormData(e.target); // grab FormData
-    $.ajax({
-      //url: 'http://httpbin.org/post',
-      url: myApp.baseUrl + '/sign-in',
-      method: 'POST',
-      contentType: false,
-      processData: false,
-      data: formData,
-    }).done(function(data) {
-      myApp.user = data.user;
-      console.log(data);
-    }).fail(function(jqxhr) {
-      console.error(jqxhr);
-    });
+  e.preventDefault();
+  var formData = new FormData(e.target);
+  $.ajax({
+    //url: 'http://httpbin.org/post',
+    url: myApp.baseUrl + '/sign-in',
+    method: 'POST',
+    contentType: false,
+    processData: false,
+    data: formData,
+  }).done(function(data) {
+    myApp.user = data.user;
+    console.log(data);
+  }).fail(function(jqxhr) {
+    console.error(jqxhr);
   });
+});
 
-// SIGN OUT
-$('#sign-out').on('submit', function(e) {
-    e.preventDefault();
-    var formData = new FormData(e.target); // grab FormData
-    $.ajax({
-      //url: 'http://httpbin.org/post',
-      url: myApp.baseUrl + '/sign-out',
+$('#sign-out').submit(function(e) {
+  e.preventDefault();
+  var formData = new FormData(e.target);
+  if (!myApp.user) {
+    console.error('Wrong!');
+    return;
+  }
+  $.ajax({
+      url: myApp.baseUrl + '/sign-out/' + myApp.user.id,
       method: 'DELETE',
+      headers: {
+        Authorization: 'Token token=' + myApp.user.token,
+      },
+      contentType: false,
+      processData: false,
+      data: formData,
+    })
+    .done(function(data) {
+      myApp.user = data.user;
+      console.log(data);
+    }).fail(function(jqxhr) {
+      console.error(jqxhr);
+    });
+});
+
+$('#change-password').submit(function(e) {
+    e.preventDefault();
+    if (!myApp.user) {
+      alert('Wrong password!');
+      return;
+    }
+    var formData = new FormData(e.target);
+    $.ajax({
+      url: myApp.baseURL + '/change-password/' + myApp.user.id,
+      // url: 'http://httpbin.org/post',
+      method: 'PATCH',
+      headers: {
+        Authorization: 'Token token=' + myApp.user.token,
+      },
       contentType: false,
       processData: false,
       data: formData,
     }).done(function(data) {
-      myApp.user = data.user;
       console.log(data);
     }).fail(function(jqxhr) {
       console.error(jqxhr);
     });
   });
 
-$('#change-password').on('submit', function(e) {
-   e.preventDefault();
-   if (!myApp.user) {
-     console.error('Wrong!');
-     return;
-   }
-
-   var formData = new FormData(e.target);
-   $.ajax({
-     // url: 'http://httpbin.org/post',
-     url: myApp.baseUrl + '/change-password/' + myApp.user.id,
-     method: 'PATCH',
-     headers: {
-       Authorization: 'Token token="bd9ed1e6d6dbaf984252c6eb026b20d1";' + myApp.user.token,
-     },
-     contentType: false,
-     processData: false,
-     data: formData,
-   }).done(function(data) {
-     console.log(data);
-   }).fail(function(jqxhr) {
-     console.error(jqxhr);
-   });
- });
+$(document).ready(() => {});
 
 module.exports = true;
